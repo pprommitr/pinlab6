f412a9c59d118d34c1c2f729a2931bc11291f1d8
pprommitr <pprommitr@gmail.com>
2022-12-09 15:39:25 +0700

    File Out lab1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num,even=0,odd=0;
+     while( num != 0){
+     cout << "Enter an integer: ";
+     cin >> num;
+       if(num%2 == 0){
+        even++;
+        }else{
+        odd++;
+     }
+     }
+      cout << "#Even numbers = "<<even-1<< endl;
+      cout << "#Odd numbers = "<<odd<< endl;
     return 0;
 }
